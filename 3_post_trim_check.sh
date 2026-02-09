9b82d2a84b49217b19fa91ce120ceeaf4f8fcfbe
StarrryNight <lausingsamuel@gmail.com>
2026-02-09 15:01:12 -0800

    finish post trim checks

---
+++ 3_post_trim_check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH --time=2:00:00
+#SBATCH --account=def-cdeboer
+#SBATCH --job-name=fastqc_raw
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --mem=16G
+#SBATCH --output=fastqc_raw_%j.out
+#SBATCH --error=fastqc_raw_%j.err
+
+module load fastqc
+
+OUTDIR=outputs/fastqc/before_trim
+DATA=outputs/fastqc/after_trim/trimmed_R1_paired.fq.gz
+DATA2=outputs/fastqc/after_trim/trimmed_R2_paired.fq.gz
+fastqc -o ${OUTDIR} -t 4 -f fastq ${DATA} ${DATA2}
