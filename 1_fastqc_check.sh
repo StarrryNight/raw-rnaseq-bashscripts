68c90349f3984c3aaf8c2d4d28f2da82dcf5cd79
StarrryNight <lausingsamuel@gmail.com>
2026-02-09 12:04:17 -0800

    finish fastqc processing

---
+++ 1_fastqc_check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH --time=2:00:00
+#SBATCH --account=def-cdeboer
+#SBATCH --job-name=fastqc_raw
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --mem=16G
+#SBATCH --output=fastqc_raw_%j.out
+#SBATCH --error=fastqc_raw_%j.err
+
+module load fastqc
+
+OUTDIR=outputs/fastqc/before_trim
+DATA=data/1951f6-DL-8_10_S13_L007_R1_001.fastq.gz
+
+fastqc -o ${OUTDIR} -t 4 -f fastq ${DATA}
